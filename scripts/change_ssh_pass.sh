482049014faea2c093f9b86aeba3696e51f0f2a6
ryomahostingph <77398623+ryomahostingph@users.noreply.github.com>
2021-08-15 18:23:29 +0800

    Delete scripts directory

--- scripts/change_ssh_pass.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-RYOMA_DIR=/usr/share/ryoma
-xterm -title "Change SSH Password" -bg black -fg white -hold -e sh $RYOMA_DIR/scripts/change_ssh_pass_script.sh &
--- scripts/start_map_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home/rathena/Desktop/rAthena
-./map-server
